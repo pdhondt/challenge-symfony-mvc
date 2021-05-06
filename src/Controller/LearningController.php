290598d04b091393f2c32337815ef84affc8e69e
Peter <dhondt.peter@gmail.com>
2021-05-06 17:17:47 +0200

    added functionality

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+session_start();
+
 class LearningController extends AbstractController
 {
     #[Route('/learning', name: 'learning')]
@@ -20,8 +22,40 @@ class LearningController extends AbstractController
     public function aboutMe(): Response
     {
         return $this->render('learning/about.html.twig', [
-
+            'name' => 'Peter'
         ]);
+    }
+
+    #[Route('/', name: 'showmyname')]
+    public function showMyName(): Response
+    {
+        if(!empty($_SESSION['name'])){
+            $name = $_SESSION['name'];
+        } else {
+            $name = 'Unknown';
+        }
+
+        return $this->render('learning/showmyname.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+    #[Route('/change-my-name', name: 'changemyname', methods: ['POST'])]
+    public function changeMyName(): Response
+    {
+        if (isset($_POST['change'])){
+            $_SESSION['name'] = $_POST['name'];
+
+            return $this->render('learning/changemyname.html.twig', [
+                'name' => $_SESSION['name'],
+            ]);
+        }
+
+        if (isset($_POST['save'])){
+            return $this->redirectToRoute('showmyname');
+        }
 
     }
+
+
 }
